284d7063fb45eceb6b4297490664ca9f4fed2d6f
Meshal <iimeshalenzi@gmail.com>
2017-02-19 22:30:31 +0300

    jj

--- src/t2/test.java
+++ src/t2/test.java
@@ -10,9 +10,6 @@ public class test {
 		
 		Patient a1 = new Patient ("saleh , "     ,      "a ", 123 , 65 ) ; 
 		Patient a2= new Patient ("moh , "  , "ss ", 3456 , 55 ) ; 
-		Patient a3 = new Patient ("ggg , "  , "sss ", 789 , 44 ) ; 
-		Patient a4 = new Patient ("xxxx , "  , "xx ", 12332 , 24 ) ; 
-
 		
 		Hospital h = new Hospital (20 ," alhbib") ;
 		
